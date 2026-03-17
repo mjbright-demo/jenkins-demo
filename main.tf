ccb9719f6fe74865743f6aa34c23a8b40287b9c1
mjbright-demo <mjbright-demo@mjbright.net>
2026-03-17 15:54:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,19 +9,22 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+
+  # Pickup from ~/.azure:
+  # subscription_id = "<your subscription id here>"
 }
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    #resource_group_name  = "RG1"
+    resource_group_name  = "studentN"
+    storage_account_name = "mjbrightc0storage"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "RG2"
-  location = "westeurope"
-}
+#resource "azurerm_resource_group" "example" {
+#  name     = "RG2"
+#  location = "westeurope"
+#}
